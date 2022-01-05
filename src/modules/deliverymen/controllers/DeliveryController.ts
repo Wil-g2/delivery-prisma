e91966d68b18b4645d4671e6dd6b0257742961c5
Willian Rezende <wil-g2@hotmail.com>
2022-01-04 22:46:49 -0300

    refactor: adjusment import use cases

--- src/modules/deliverymen/controllers/DeliveryController.ts
+++ src/modules/deliverymen/controllers/DeliveryController.ts
@@ -1,24 +1,30 @@
 import { Request, Response } from "express";
-import CreateDelivery from "../useCases/CreateDelivery";
-import SessionDelivery from "../useCases/SessionDelivery";
-import ShowAllDelivery from "../useCases/ShowAllDelivery";
+import CreateDeliveryUseCase from "../useCases/CreateDeliveryUseCase";
+import SessionDeliveryUseCase from "../useCases/SessionDeliveryUseCase";
+import ShowAllDeliveryUseCase from "../useCases/ShowAllDeliveryUseCase";
 
 class DeliveryController {
   public async store(req: Request, res: Response) {
     const { username, password } = req.body;
-    const deliveryman = await CreateDelivery.execute({ username, password });
+    const deliveryman = await CreateDeliveryUseCase.execute({
+      username,
+      password,
+    });
     return res.status(201).json({ deliveryman });
   }
 
   public async session(request: Request, response: Response) {
     const { username, password } = request.body;
 
-    const session = await SessionDelivery.execute({ username, password });
+    const session = await SessionDeliveryUseCase.execute({
+      username,
+      password,
+    });
     return response.json(session);
   }
 
   public async index(request: Request, response: Response) {
-    const deliverymen = await ShowAllDelivery.execute();
+    const deliverymen = await ShowAllDeliveryUseCase.execute();
     return response.json(deliverymen);
   }
 }
